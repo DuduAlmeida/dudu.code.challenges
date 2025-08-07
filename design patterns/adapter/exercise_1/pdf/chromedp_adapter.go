0ecd138be3b1b5fce5e1a2ed6b5d3ab5d47c4c80
Eduardo Almeida <contato.eduardopex@hotmail.com>
2025-08-07 09:21:40 -0300

    chore: remove some comments

--- design patterns/adapter/exercise_1/pdf/chromedp_adapter.go
+++ design patterns/adapter/exercise_1/pdf/chromedp_adapter.go
@@ -22,36 +22,23 @@ func (w *ChromedpAdapter) Generate(filename string, htmlContent string) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 
-	// Cria o contexto do chromedp.
-	// O último argumento `chromedp.Headless` é opcional, mas garante que o navegador
-	// não seja exibido em uma janela.
 	ctx, cancel = chromedp.NewContext(ctx, chromedp.WithLogf(log.Printf))
 	defer cancel()
 
-	// Cria uma slice de bytes para armazenar o conteúdo do PDF
 	var buf []byte
 
-	// Executa as tarefas no navegador headless
 	err := chromedp.Run(ctx,
-		// Define o conteúdo HTML da página
 		chromedp.Navigate("about:blank"),
 		chromedp.ActionFunc(func(ctx context.Context) error {
-			// Adiciona o conteúdo HTML à página
 			frameTree, err := page.GetFrameTree().Do(ctx)
 			if err != nil {
 				return err
 			}
 			return page.SetDocumentContent(frameTree.Frame.ID, htmlContent).Do(ctx)
 		}),
-		// Espera que a página esteja completamente carregada para evitar PDFs incompletos
-		chromedp.Sleep(1*time.Second), // Pequena pausa para garantir a renderização
+		chromedp.Sleep(1*time.Second),
 
-		// Configura e imprime a página como PDF
 		chromedp.ActionFunc(func(ctx context.Context) error {
-			// page.PrintToPDF gera o PDF.
-			// Configurações como PageRanges, Landscape, PaperSize podem ser alteradas aqui.
-			// Por exemplo: landscape := true
-			// p := page.PrintToPDF().WithLandscape(landscape)
 			p := page.PrintToPDF()
 
 			var err error
@@ -64,7 +51,6 @@ func (w *ChromedpAdapter) Generate(filename string, htmlContent string) error {
 		return fmt.Errorf("falha na geração do PDF: %w", err)
 	}
 
-	// Salva o buffer em um arquivo
 	err = os.WriteFile(filename, buf, 0644)
 	if err != nil {
 		return fmt.Errorf("falha ao salvar o PDF: %w", err)
